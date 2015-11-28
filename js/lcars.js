ee2bcbb02a84316b6511e07dc0cd3c61a3419478
mttaggart <michael.t.taggart@gmail.com>
2015-11-27 23:54:31 -0800

    Finish most features

--- js/lcars.js
+++ js/lcars.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
   $("input[type='checkbox'],input[type='radio']")
     .parent()
-    .prepend("<svg class='lcars'><rect class='lcars-select'></rect></svg>")
-  $("input[type='checkbox'],input[type='radio']")
+    .prepend("<svg class='lcars-svg'><rect class='lcars-control'></rect></svg>");
+  $(".lcars-select-label").click(function(){
+    $(this).children("select").focus();
+  });
+  $(".lcars-accordion-heading")
+  .append(" <span class='fa fa-caret-down'></span>")
+  .click(function(){
+    $(this).parent().children(".lcars-accordion-content").slideToggle(200);
+  });
+  $(".dropdown").click(function(){
+    $(this).children(".lcars-dropdown").slideToggle(200).
+    mouseleave(function(){
+      $(this).slideUp(200);
+    });
+  })
+  $("input[type='checkbox']")
     .change(function(){
     if($(this).prop("checked")) {
       $(this).parent().addClass("checked");
@@ -11,6 +25,9 @@ $(document).ready(function(){
       $(this).parent().removeClass("checked");
       $(this).parent().find("rect").css("fill","black");
     }
-    console.log($(this).parent().html())
+  });
+  $("input[type='radio']").change(function(){
+    $(this).parent().parent().find("rect").css("fill","black");
+    $(this).parent().find("rect").css("fill","yellow");
   });
 });
